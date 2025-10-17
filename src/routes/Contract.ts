6f66e579d8ca31f99eef8ea7a2959711260221e4
Kchiinaphat <you@example.com>
2025-10-17 14:08:46 +0700

    argument false

--- src/routes/Contract.ts
+++ src/routes/Contract.ts
@@ -5,17 +5,15 @@ import sendEmail from '../sendEmail';
 const router = Router();
 
 router.post('/', async (req: Request, res: Response) => {
-
   if (!req.body.firstName || !req.body.email) {
-  return res.status(400).json({ success: false, message: "กรอกข้อมูลไม่ครบ" });
-}
+    return res.status(400).json({ success: false, message: "กรอกข้อมูลไม่ครบ" });
+  }
 
   try {
     const newContract = new Contact(req.body);
     await newContract.save();
 
-
-    await sendEmail(process.env.COMPANY_EMAIL || '', {
+    await sendEmail({
       subject: 'มีคนติดต่อผ่านเว็บไซต์',
       text: `
 ชื่อ: ${req.body.firstName} ${req.body.lastName}
@@ -24,12 +22,13 @@ router.post('/', async (req: Request, res: Response) => {
 เบอร์โทร: ${req.body.phone}
 บริษัท: ${req.body.company || '-'}
       `,
-
     });
+
     res.status(201).json({ success: true, message: 'ส่งข้อมูลเรียบร้อย' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: 'เกิดข้อผิดพลาดในการส่งข้อมูล' });
   }
 });
+
 export default router;
